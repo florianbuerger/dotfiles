60ff44cd078a6bee9a6bb42d1fcdd7c16e0ed85f
Florian Buerger <florian@florianbuerger.com>
2017-07-11 15:02:36 -0600

    Fix zsh for iTerm

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -29,5 +29,5 @@ function machine_info() {
 	fi
 }
 
-PROMPT='$(machine_info)%{$fg[blue]%}%c%{$reset_color%}$ '
+PROMPT='🐶 $(machine_info)%{$fg[blue]%}%c%{$reset_color%}: '
 RPROMPT='$(git_prompt_info)'
