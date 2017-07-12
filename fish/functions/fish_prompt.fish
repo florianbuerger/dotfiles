e262f20158f348754b24035af2fc9ae3b26ec0f2
Florian Buerger <florian@florianbuerger.com>
2017-07-12 12:53:54 -0600

    Show user & machine when in SSH

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,7 +1,18 @@
+function machine_info
+  if begin; set -q SSH_CLIENT; or set -q SSH_TTY; end 
+    set_color cyan
+    echo -n -s "$USER" @ (prompt_hostname)
+    set_color normal
+    echo -n ':'
+  end
+end
+
 function fish_prompt
 	set_color $fish_color_cwd
-    echo -n '🐼  '
-    echo -n (prompt_pwd)
-    set_color normal
-    echo -n ' >> '
+  echo -n '🐼  '
+  machine_info
+	set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set_color normal
+  echo -n ' >> '
 end
