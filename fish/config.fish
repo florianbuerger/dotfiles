c926fe910db5cf016bc2012ac0629de21b57af22
Florian Buerger <florian@florianbuerger.com>
2017-07-14 13:26:52 -0600

    Improve colors for light term

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,45 @@
-set fish_greeting ''
+set fish_greeting
+set -g fish_prompt_pwd_dir_length 0
 
-set -x EDITOR 'vim'
+set -x LANG "en_US.UTF-8"
+set -x LSCOLORS "ExfxcxdxBxegedabagacad"
+
+set -x EDITOR "vim"
+set -x VISUAL "vim"
+set -x PAGER "less"
+set -x LESS "-R"
+
+switch (uname)
+case Darwin
+  alias ls "ls -G"
+case "*"
+  alias ls "ls --color=auto"
+end
+
+alias l "ls"
+alias ll "ls -hl"
+alias la "ll -a"
 
 set -x PATH $HOME/.bin /usr/local/sbin $PATH
 
 # Ruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
+
+set -l primary_color cyan --bold
+set -l secondary_color yellow --bold
+set -l error_color red
+
+set fish_color_normal $secondary_color
+set fish_color_command $primary_color
+set fish_color_quote $secondary_color
+set fish_color_redirection $secondary_color
+set fish_color_end $secondary_color
+set fish_color_error $error_color
+set fish_color_param $secondary_color
+set fish_color_comment $secondary_color
+set fish_color_match $secondary_color
+set fish_color_search_match $secondary_color
+set fish_color_operator $secondary_color
+set fish_color_escape $secondary_color
+set fish_color_cwd $secondary_color
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,41 @@
-function machine_info
-  if begin; set -q SSH_CLIENT; or set -q SSH_TTY; end 
-    set_color cyan
-    echo -n -s "$USER" @ (prompt_hostname)
-    set_color normal
-    echo -n ':'
-  end
-end
+set c_reset (set_color normal)
+set c_cyan (set_color cyan)
+set c_light (set_color aaa)
+set c_green (set_color green)
+set c_magenta (set_color magenta)
+
+# Fish git prompt
+# set __fish_git_prompt_showdirtystate 'false'
+set __fish_git_prompt_color_branch aaa
+
+# Status Chars
+# set __fish_git_prompt_char_dirtystate '*'
 
 function fish_prompt
-	set_color $fish_color_cwd
-  echo -n '🐶  '
-  machine_info
-	set_color $fish_color_cwd
-  echo -n (prompt_pwd)
-  set_color normal
-  echo -n '> '
+
+  printf "%s" $c_reset
+
+  set separator "$c_reset :: "
+
+# User & hose only when in SSH
+if begin; set -q SSH_CLIENT; or set -q SSH_TTY; end 
+  printf "%s%s" $c_bold (whoami)
+  printf "%s" $separator
+  printf "%s%s" $c_green (hostname -s)
+  printf "%s" $separator
+end
+
+  printf "%s%s" $c_bold (prompt_pwd)
+  printf "%s" (__fish_git_prompt " :: %s") 
+
+  echo
+
+  printf "%s%s " "\$" $c_reset
+end
+
+# I would totally put this in a separate file, but because functions are
+# lazy-loaded, the prompt won't actually register this handler until it's
+# defined. But fish *knows* to load `fish_prompt` so we're ok if it's over here.
+function postexec --on-event fish_postexec
+  echo
 end
