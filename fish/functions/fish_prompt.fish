9f6241e095b61cd06ffc1c9fc07c81f70bdecf7b
Florian Buerger <florian@florianbuerger.com>
2017-07-26 14:46:45 +0200

    Show different emoji when on server

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,17 @@
 function machine_info
   if begin; set -q SSH_CLIENT; or set -q SSH_TTY; end 
+    echo -n '🌎  :: '
     set_color cyan
     echo -n -s "$USER" @ (prompt_hostname)
     set_color normal
-    echo -n ':'
+    echo -n '::'
+  else
+    echo -n '🐼  '
   end
 end
 
 function fish_prompt
 	set_color $fish_color_cwd
-  echo -n '🐼  '
   machine_info
 	set_color $fish_color_cwd
   echo -n (prompt_pwd)
