c987dc5c6cd71bbfc82963476a69f8e5d1eaf7b3
Florian Buerger <florian@florianbuerger.com>
2019-01-25 14:40:00 +0200

    Remove MS tools

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,4 @@ set -x LANG en_US.UTF-8
 
 set -x EDITOR vim
 
-set -gx PATH ~/.bin /usr/local/opt/mysql@5.5/bin /usr/local/opt/qt@5.5/bin /usr/local/sbin $PATH
-
-source ~/.asdf/asdf.fish
+set -gx PATH ~/.bin /usr/local/sbin $PATH
