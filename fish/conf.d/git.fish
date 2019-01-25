b12bc28d52d46ef9d70d1a1c8e39dd2109bf9109
Florian Buerger <florian@florianbuerger.com>
2019-01-25 14:40:42 +0200

    Use system ruby

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -7,6 +7,7 @@ abbr -a gt gittower .
 abbr -a gf git fetch
 abbr -a gs git status -sb
 abbr -a g git status -sb
+abbr -a gc git commit
 
 abbr -a gm git merge --ff-only
 abbr -a gmn git merge --no-ff
--- fish/conf.d/ruby.fish
+++ fish/conf.d/ruby.fish
@@ -1,3 +1,4 @@
-source ~/.asdf/asdf.fish
+set -gx PATH ~/.gem/ruby/2.3.0/bin $PATH
+
 abbr -a bi bundle install
 abbr -a be bundle exec
