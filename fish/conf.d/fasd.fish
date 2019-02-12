dda5ae6aed4027c0ff57bdfc2e9d79b7f9c2940d
Florian Buerger <florian@florianbuerger.com>
2019-02-12 11:22:32 +0200

    Fix fasd in fish sell

--- fish/conf.d/fasd.fish
+++ fish/conf.d/fasd.fish
@@ -1,7 +1,16 @@
-function _run_fasd --on-event preexec
-  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
+function _run_fasd --on-event fish_preexec
+	fasd --proc (fasd --sanitize $argv | tr -s ' ' \n) > /dev/null 2>&1
 end
 
 function j
-  cd (fasd -d -e 'printf %s' "$argv")
+    set -l dir (fasd -de "printf %s" "$argv")
+    if test "$dir" = ""
+        echo "no matching directory"
+        return 1
+    end
+    cd $dir
+end
+
+function e
+    fasd -fe "$EDITOR" "$argv"
 end
